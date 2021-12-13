f30458befdb033b9ef2bb2f249b0fc3602cd51e3
Alexandre <alexandre.j.simon@gmail.com>
2021-12-12 23:33:44 -0300

    update readme and set monitor

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Portfolio Investimento
+Projeto desenvolvido em `golag` na versão `1.17`
 ## Instalação das dependencias
 Para instalar as dependencias basta rodar o comando `make deps`
 
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"sort"
+	"time"
 
 	"github.com/AlexandreJSimon/portfolio-investimento/services"
 	"github.com/AlexandreJSimon/portfolio-investimento/services/calc"
@@ -23,7 +24,7 @@ func main() {
 	if *fileName == "" {
 		panic("Arquivo não informado !")
 	}
-
+	start := time.Now()
 	//Abrindo o arquivo
 	in, err := os.Open(*fileName)
 	if err != nil {
@@ -49,29 +50,44 @@ func main() {
 	returnRiskCalc := calc.NewService(calc.ServiceInput{Assets: assets})
 
 	returnRisk := returnRiskCalc.Run()
+	elapsed := time.Since(start)
+	fmt.Println(fmt.Sprintf("Tempo de leitura do arquivo e calculos %s", elapsed))
+	fmt.Println()
 
+	start = time.Now()
 	// Ordena dados
 	sort.Slice(returnRisk, func(i, j int) bool {
 		return returnRisk[i].ReturnRisk < returnRisk[j].ReturnRisk
 	})
+	elapsed = time.Since(start)
+	fmt.Println(fmt.Sprintf("Tempo de ordenação dos dados %s", elapsed))
+	fmt.Println()
 
 	//Printa solução gulosa
 	fmt.Println("Solução com algoritmo Guloso:")
+	start = time.Now()
 	algGreedy := greedy.NewService(greedy.ServiceInput{ReturnRisk: returnRisk})
 	gr, grr := algGreedy.Run()
 	for _, v := range gr {
 		fmt.Println(fmt.Sprintf("ativo %s com peso %f", v.Asset, v.Weight))
 	}
 	fmt.Println(fmt.Sprintf("Risco retorno total %f", grr))
+
+	elapsed = time.Since(start)
+	fmt.Println(fmt.Sprintf("Risco retorno total %f", grr))
+	fmt.Println(fmt.Sprintf("Tempo de execução da solução gulosa %s", elapsed))
 	fmt.Println()
 
 	//Printa solucao aleatoria
 	fmt.Println("Solução com pesos aleatorios:")
+	start = time.Now()
 	algRand := randon.NewService((randon.ServiceInput{ReturnRisk: returnRisk}))
 	al, alr := algRand.Run()
 	for _, v := range al {
 		fmt.Println(fmt.Sprintf("ativo %s com peso %f", v.Asset, v.Weight))
 	}
 	fmt.Println(fmt.Sprintf("Risco retorno total %f", alr))
+	elapsed = time.Since(start)
+	fmt.Println(fmt.Sprintf("Tempo de execução da solução aleatoria %s", elapsed))
 	fmt.Println()
 }
